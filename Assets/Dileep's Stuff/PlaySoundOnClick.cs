84e4b7969c06268229bcfdee0d382a5e40733e3d
Dileep Rajput <dileep_rajput@yahoo.com>
2018-04-12 16:46:56 -0400

    Add question menu

---
+++ Assets/Dileep's Stuff/PlaySoundOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnClick : MonoBehaviour {
+
+	private AudioSource audio;
+
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	public void playSound() {
+		if (audio.isPlaying) {
+			audio.Stop ();
+		}
+
+		audio.Play ();
+	}
+}
