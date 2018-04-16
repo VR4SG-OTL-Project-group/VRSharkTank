db46b96af513c6c8e5e3234ddf604ec3dcc16dfc
Ryan Zeng <zeng.ryan2@gmail.com>
2018-04-16 09:19:18 -0400

    Added introduction and tablet

---
+++ Assets/RyanZ Assets/EndIntro.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndIntro : MonoBehaviour {
+
+	private bool isPlaying = false;
+	public AudioClip dialogueClip;
+	public float audioLength;
+	// Update is called once per frame
+	void Update () {
+		/*if(Input.GetMouseButtonDown(0)){
+		//if (Input.GetKeyDown (KeyCode.Space)) {
+			DialogueManager.Instance.beginDialogue (dialogueClip);
+		}*/
+	}
+
+	public void playDialogueClip(){
+		if (!isPlaying) {
+			DialogueManager.Instance.beginDialogue (dialogueClip);
+			isPlaying = true;
+			StartCoroutine (finishAudioClip ());
+		}
+	}
+
+	private IEnumerator finishAudioClip(){
+		yield return new WaitForSeconds (audioLength + 5);
+		SceneManager.LoadScene ("MainScene");
+	}
+
+}
