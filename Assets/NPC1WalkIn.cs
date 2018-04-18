f1478e95b6af3177e28bc4425c2b31c305ce46de
Ryan Fernandez <rfernandez754@gmail.com>
2018-04-18 18:28:43 -0400

    added npcs

---
+++ Assets/NPC1WalkIn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class NPC1WalkIn : MonoBehaviour
+{
+
+    private Animator anim;
+    public int npc1SpeechTime = 10;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("WalkFWD");
+        StartCoroutine(WaitCoroutine()); // Remove if questions are used
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        //This is a coroutine
+
+        yield return new WaitForSeconds(npc1SpeechTime);
+
+        anim.Play("WalkForwardTurnRight_NtrlShort 2");
+        GameObject npc2 = GameObject.Find("GreenSuitMan");
+        npc2.GetComponent<NPC2WalkIn>().WalkIn();
+    }
+
+    public void ExitRoom() // Called when user is done asking questions
+    {
+        anim.Play("WalkForwardTurnRight_NtrlShort 2");
+        GameObject npc2 = GameObject.Find("GreenSuitMan");
+        npc2.GetComponent<NPC2WalkIn>().WalkIn();
+    }
+}
---
+++ Assets/NPC2WalkIn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC2WalkIn : MonoBehaviour {
+
+    private Animator anim;
+    public int npc2SpeechTime = 10;
+
+    public void WalkIn()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(WaitToEnterCoroutine());
+    }
+
+    IEnumerator WaitToEnterCoroutine()
+    {
+        //This is a coroutine
+        yield return new WaitForSeconds(5);
+        anim.Play("WalkFWD");
+        StartCoroutine(WaitToExitCoroutine()); // Remove if questions are used
+    }
+
+    IEnumerator WaitToExitCoroutine()
+    {
+        //This is a coroutine
+
+        yield return new WaitForSeconds(npc2SpeechTime);
+
+        anim.Play("WalkForwardTurnRight_NtrlShort 2");
+        if (this.gameObject != GameObject.Find("RedSuitMan"))
+        {
+            GameObject npc3 = GameObject.Find("RedSuitMan");
+            npc3.GetComponent<NPC2WalkIn>().WalkIn();
+        }
+    }
+
+    public void ExitRoom() // Called when user is done asking questions
+    {
+        anim.Play("WalkForwardTurnRight_NtrlShort 2");
+        if (this.gameObject != GameObject.Find("RedSuitMan"))
+        {
+            GameObject npc3 = GameObject.Find("RedSuitMan");
+            npc3.GetComponent<NPC2WalkIn>().WalkIn();
+        }
+    }
+}
