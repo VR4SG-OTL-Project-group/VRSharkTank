50ad7aaf9032c94e1ceb68a18baaeb461b0b8316
lowens97 <32307075+lowens97@users.noreply.github.com>
2018-04-30 20:42:16 -0400

    Add audio to question menus

--- Assets/Dileep's Stuff/PlaySoundOnClick.cs
+++ Assets/Dileep's Stuff/PlaySoundOnClick.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class PlaySoundOnClick : MonoBehaviour {
 
 	private AudioSource audio;
+    //public GameObject menu;
+    private QuestionIsPlaying isPlayingScript;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		audio = GetComponent<AudioSource> ();
+        isPlayingScript = gameObject.GetComponentInParent<QuestionIsPlaying>();
 	}
 	
 	public void playSound() {
+        if(isPlayingScript.questionPlaying)
+        {
+            return;
+        }
+
 		if (audio.isPlaying) {
 			audio.Stop ();
 		}
 
+        isPlayingScript.questionPlaying = true;
 		audio.Play ();
+        StartCoroutine(resetQuestionPlaying());
 	}
+
+    private IEnumerator resetQuestionPlaying()
+    {
+        yield return new WaitForSeconds(audio.clip.length);
+        isPlayingScript.questionPlaying = false;
+    }
 }
